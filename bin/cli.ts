db0d5bbf8c9eef48402d24ee0e870ed7aaf0fe5f
Brian Graham <brian@statagroup.com>
2018-04-28 17:13:08 +0200

    We don't really need all this stuff

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,10 +1,5 @@
 import * as program from 'commander'
-import * as schedule from 'node-schedule'
 import { logger } from '../src/logger'
-
-function heartBeat () {
-    logger.info('Heartbeat: Application is still running.')
-}
 
 async function webstart () {
     try {
@@ -15,21 +10,8 @@ async function webstart () {
       return
     }
     logger.info('Starting webserver')
-    schedule.scheduleJob('0 */20 * * * * *', heartBeat)
+
+    // TODO actually start a web server
 }
 
-program
-    .version('0.0.0')
-
-program
-    .command('web:start')
-    .description('Run the web server')
-    .action(webstart)
-
-program
-    .command('*')
-    .action(() => {
-      program.outputHelp()
-    })
-
-program.parse(process.argv)
+webstart()
