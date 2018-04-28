2e32d867644c619384ba28c6ef378a4aaa5a2500
Brian Graham <brian@statagroup.com>
2018-04-28 17:02:49 +0200

    No Crypto? No Server.

--- bin/cli.ts
+++ bin/cli.ts
@@ -7,6 +7,13 @@ function heartBeat () {
 }
 
 async function webstart () {
+    try {
+      crypto = require('crypto');
+    } catch (err) {
+      logger.emerg('Node on this server was build without Crypto support. I refuse to let you use this for your own good.')
+      process.exit(1)
+      return
+    }
     logger.info('Starting webserver')
     schedule.scheduleJob('0 */20 * * * * *', heartBeat)
 }
