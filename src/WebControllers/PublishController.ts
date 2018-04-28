e27b8a2e2386b9f6a8b5d2b6a96f529830a2e3ba
Brian Graham <brian@statagroup.com>
2018-04-28 17:50:25 +0200

    Also this TODO is nice

--- src/WebControllers/PublishController.ts
+++ src/WebControllers/PublishController.ts
@@ -2,5 +2,7 @@ import { WebControllerInterface } from './WebControllerInterface'
 
 export class PublishController() {
   public execute(request, response) {
+    // TODO return a redirect, also create a
+    // page to show the user what URL to pass
   }
 }
