e7f6242c3390ec34fcf95202c1f636d0f6ee0753
Brian Graham <Incognito@users.noreply.github.com>
2018-04-29 18:02:07 +0200

    Update cli.ts

--- bin/cli.ts
+++ bin/cli.ts
@@ -5,7 +5,7 @@ function webstart () {
   try {
     require('crypto');
   } catch (err) {
-    logger.emerg('Node on this server was build without Crypto support. I refuse to let you use this for your own good.', err);
+    logger.emerg('Node on this server was built without Crypto support. I refuse to let you use this for your own good.', err);
     process.exit(1);
     return;
   }
