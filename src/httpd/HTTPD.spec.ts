6390f111d3a2358330ddc049ffb5f677645baddc
Brian Graham <brian@statagroup.com>
2018-05-05 23:10:22 +0200

    Cover the remainder of httpd

--- src/httpd/HTTPD.spec.ts
+++ src/httpd/HTTPD.spec.ts
@@ -1,3 +1,4 @@
+import { Writable } from 'stream';
 import { HTTPD } from './HTTPD';
 
 describe('HTTPD Spec', function() {
@@ -22,11 +23,6 @@ describe('HTTPD Spec', function() {
 
     expect(serverSetTimeoutMock.mock.calls.length).toBe(1);
     expect(serverSetTimeoutMock.mock.calls[0][0]).toBe(31337);
-
-    // todo: test on client error
-    // todo: test client error raises error
-    // todo: test client error calls socket.end
-    // todo: test on request calls request handler
 
     expect(sut).toEqual(expect.any(HTTPD));
   });
@@ -55,4 +51,87 @@ describe('HTTPD Spec', function() {
     expect(serverListenMock.mock.calls.length).toBe(1);
     expect(serverListenMock.mock.calls[0][0]).toBe(1337);
   });
+
+  it('should log error on client error', async function() {
+    jest.mock('http');
+    const serverStreamMock = <any> new Writable();
+    serverStreamMock.setTimeout = jest.fn();
+
+    const requestHandlerMock = { process: jest.fn() };
+    const loggerMock = <any> { info: jest.fn(), debug: jest.fn(), error: jest.fn() };
+
+    // tslint:disable-next-line
+    new HTTPD(
+      loggerMock,
+      serverStreamMock,
+      <any> requestHandlerMock,
+      1337,
+      31337
+    );
+    const endMock = jest.fn();
+    serverStreamMock.emit('clientError', <any> 'error', <any> { end: endMock });
+
+    await new Promise((resolve) => {
+      process.nextTick(resolve);
+    });
+    expect(loggerMock.error).toBeCalled();
+    expect(endMock).toBeCalledWith('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
+
+  it('should log error on client error without error message', async function() {
+    jest.mock('http');
+    const serverStreamMock = <any> new Writable();
+    serverStreamMock.setTimeout = jest.fn();
+
+    const requestHandlerMock = { process: jest.fn() };
+    const loggerMock = <any> { info: jest.fn(), debug: jest.fn(), error: jest.fn() };
+
+    // tslint:disable-next-line
+    new HTTPD(
+      loggerMock,
+      serverStreamMock,
+      <any> requestHandlerMock,
+      1337,
+      31337
+    );
+    const endMock = jest.fn();
+    serverStreamMock.emit('clientError', <any> false, <any> { end: endMock });
+
+    await new Promise((resolve) => {
+      process.nextTick(resolve);
+    });
+    expect(loggerMock.error).not.toBeCalled();
+    expect(loggerMock.info).toBeCalled();
+    expect(endMock).toBeCalledWith('HTTP/1.1 400 Bad Request\r\n\r\n');
+  });
+
+  it('should log error on client error', async function() {
+    jest.mock('http');
+    const serverStreamMock = <any> new Writable();
+    serverStreamMock.setTimeout = jest.fn();
+
+    const requestHandlerMock = { process: jest.fn() };
+    const loggerMock = <any> { info: jest.fn(), debug: jest.fn(), error: jest.fn() };
+
+    // tslint:disable-next-line
+    new HTTPD(
+      loggerMock,
+      serverStreamMock,
+      <any> requestHandlerMock,
+      1337,
+      31337
+    );
+
+    const requestMock = <any> {};
+    const responseMock = <any> {};
+    serverStreamMock.emit('request', requestMock, responseMock);
+
+    await new Promise((resolve) => {
+      process.nextTick(resolve);
+    });
+
+    expect(loggerMock.debug).toBeCalled();
+    expect(requestHandlerMock.process).toBeCalledWith(requestMock, responseMock);
+  });
+
 });
