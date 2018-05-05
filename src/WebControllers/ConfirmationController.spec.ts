85c16649dacb91f2ef6a6c897bb520000923b46d
Brian Graham <brian@statagroup.com>
2018-05-05 18:22:57 +0200

    Cover streams also

--- src/WebControllers/ConfirmationController.spec.ts
+++ src/WebControllers/ConfirmationController.spec.ts
@@ -1,14 +1,13 @@
+import { Writable } from 'stream'
+import { Readable } from 'stream'
 import { ConfirmationController } from './ConfirmationController';
 
 describe('ConfirmationController Spec', function() {
   it('should have handlers for the request upload', function() {
-
     const secretStoreMock = { set: jest.fn() };
-
     const identity = identity => identity;
-
     const sut = new ConfirmationController(
-      { KEY_NAME_RANDOM_BYTES: 0 },
+      { KEY_NAME_RANDOM_BYTES: 1, MAX_UPLOAD_KB: 1 },
       identity,
       secretStoreMock,jest.fn,
       identity
@@ -23,10 +22,63 @@ describe('ConfirmationController Spec', function() {
       writeHead: jest.fn(),
       end: jest.fn()
     };
-    // todo test reqeuest.on(data) and reqeuest.on(end)
 
     sut.execute(mockRequest, mockResponse);
     expect(mockRequest.on).toBeCalledWith('data', expect.any(Function));
     expect(mockRequest.on).toBeCalledWith('end', expect.any(Function));
+  });
+
+  it('should process an upload', async function() {
+    const secretStoreMock = { set: jest.fn() };
+    const identity = identity => identity;
+    const sut = new ConfirmationController(
+      { KEY_NAME_RANDOM_BYTES: 1, MAX_UPLOAD_KB: 1 },
+      identity,
+      secretStoreMock,jest.fn,
+      identity
+    );
+
+    const requestStreamMock = new Readable()
+    requestStreamMock.push('some-normal-payload')
+    requestStreamMock.push(null)
+
+    const mockResponse = {
+      writeHead: jest.fn(),
+      end: jest.fn()
+    };
+    await sut.execute(requestStreamMock, mockResponse);
+
+    expect(mockResponse.writeHead).toBeCalledWith(200, { 'Content-Type': 'text/html' });
+    expect(mockResponse.end).toBeCalledWith('https://example.com/fetch?key=');
+  });
+
+  it('should fail large uploads', async function() {
+    const secretStoreMock = { set: jest.fn() };
+    const identity = identity => identity;
+    const sut = new ConfirmationController(
+      { KEY_NAME_RANDOM_BYTES: 1, MAX_UPLOAD_KB: 1 },
+      identity,
+      secretStoreMock,jest.fn,
+      identity
+    );
+
+    const requestStreamMock = new Readable();
+    requestStreamMock.push("x".repeat(1026)); // more than max_upload_kb
+    requestStreamMock.push(null);
+    requestStreamMock.connection = { destroy: jest.fn() };
+
+    const mockResponse = {
+      writeHead: jest.fn(),
+      end: jest.fn()
+    };
+
+    try{
+      await sut.execute(requestStreamMock, mockResponse);
+      expect(true).toEqual(false); // Fail test if there is no uncaught rejection
+    } catch (e) {
+      expect(true).toEqual(true);
+    }
+
+    expect(requestStreamMock.connection.destroy).toBeCalled();
   });
 });
--- src/WebControllers/ConfirmationController.ts
+++ src/WebControllers/ConfirmationController.ts
@@ -7,7 +7,7 @@ import { StorageInterface } from '../one-time-secret/StorageInterface';
 
 export class ConfirmationController implements WebControllerInterface {
   public constructor(
-    private env: { KEY_NAME_RANDOM_BYTES: number, DOMAIN: string },
+    private env: { KEY_NAME_RANDOM_BYTES: number, DOMAIN: string , MAX_UPLOAD_KB: number },
     private render: Function,
     private secretStore: StorageInterface,
     private generateNewSlug: Function,
@@ -15,31 +15,34 @@ export class ConfirmationController implements WebControllerInterface {
 
   public execute(request: IncomingMessage, response: ServerResponse) {
     const secretKey = this.generateNewSlug(this.env.KEY_NAME_RANDOM_BYTES);
-
-    let body = '';
-    request.on('data', function (data) {
-      body += data;
-      if (body.length > 1024 * 128) { // 128 kb // TODO set as envvar
-        request.connection.destroy();
-      }
-    });
-
-    request.on('end', () => {
-      const parsedBody = querystring.parse(body);
-
-      this.secretStore.set(secretKey, <{secret: string}> parsedBody , +parsedBody.ttl);
-
-      const secretUrl = nodeUrl.format({
-        protocol: 'https',
-        hostname: this.env.DOMAIN,
-        pathname: '/fetch',
-        query: {
-          key: secretKey
+    return new Promise((resolve, reject)=>{
+      let body = '';
+      request.on('data', (data) => {
+        body += data;
+        if (body.length > 1024 * this.env.MAX_UPLOAD_KB) { // 128 kb // TODO set as envvar
+          request.connection.destroy();
+          reject()
         }
       });
 
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(this.render(secretUrl));
-    });
-  }
+      request.on('end', () => {
+        const parsedBody = querystring.parse(body);
+
+        this.secretStore.set(secretKey, <{secret: string}> parsedBody , +parsedBody.ttl);
+
+        const secretUrl = nodeUrl.format({
+          protocol: 'https',
+          hostname: this.env.DOMAIN,
+          pathname: '/fetch',
+          query: {
+            key: secretKey
+          }
+        });
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(this.render(secretUrl));
+        resolve()
+      });
+    }
+  })
 }
